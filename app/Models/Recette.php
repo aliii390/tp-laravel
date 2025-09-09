02c4cf2b78ac24d701cb50772279ac83df5c5e53
Ali <alli.said.errahmani@gmail.com>
2025-09-09 12:12:48 +0200

    add: rajout des liaison

--- app/Models/Recette.php
+++ app/Models/Recette.php
@@ -4,15 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Recette extends Model
 {
     use HasFactory;
-
+    
     protected $fillable = [
+
         'name',
         'preparationTime',
         'cookingTime',
         'serves'
     ];
+
+
+      
+
+    public function ingredients(): BelongsToMany
+    {
+        return $this->belongsToMany(Ingredient::class);
+    }
 }
